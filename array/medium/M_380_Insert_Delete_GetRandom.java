46df512b5ca37297dfc43743b6d5c1febe14bbbd
Mohammad Wasim khan <37899903+mdwasimkhan@users.noreply.github.com>
2023-11-06 22:05:32 +0530

    Leetcode - https://leetcode.com/problems/insert-delete-getrandom-o1

---
+++ array/medium/M_380_Insert_Delete_GetRandom.java
@@ -0,0 +1,81 @@
+package learn.dsa.array.medium;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+/*
+ * Leetcode - https://leetcode.com/problems/insert-delete-getrandom-o1/description/
+ * Implement the RandomizedSet class:
+
+	RandomizedSet() Initializes the RandomizedSet object.
+	bool insert(int val) Inserts an item val into the set if not present. Returns true if the item was not present, false otherwise.
+	bool remove(int val) Removes an item val from the set if present. Returns true if the item was present, false otherwise.
+	int getRandom() Returns a random element from the current set of elements (it's guaranteed that at least one element exists when this method is called). Each element must have the same probability of being returned.
+	You must implement the functions of the class such that each function works in average O(1) time complexity.
+	
+	Example 1:
+	
+	Input
+	["RandomizedSet", "insert", "remove", "insert", "getRandom", "remove", "insert", "getRandom"]
+	[[], [1], [2], [2], [], [1], [2], []]
+	Output
+	[null, true, false, true, 2, true, false, 2]
+	
+	Explanation
+	RandomizedSet randomizedSet = new RandomizedSet();
+	randomizedSet.insert(1); // Inserts 1 to the set. Returns true as 1 was inserted successfully.
+	randomizedSet.remove(2); // Returns false as 2 does not exist in the set.
+	randomizedSet.insert(2); // Inserts 2 to the set, returns true. Set now contains [1,2].
+	randomizedSet.getRandom(); // getRandom() should return either 1 or 2 randomly.
+	randomizedSet.remove(1); // Removes 1 from the set, returns true. Set now contains [2].
+	randomizedSet.insert(2); // 2 was already in the set, so return false.
+	randomizedSet.getRandom(); // Since 2 is the only number in the set, getRandom() will always return 2.
+ */
+public class M_380_Insert_Delete_GetRandom {
+
+	List<Integer> nums;
+    Map<Integer, Integer> indexMap;
+    Random random;
+
+    public M_380_Insert_Delete_GetRandom() {
+        nums = new ArrayList<>();
+        indexMap = new HashMap<>();
+        random = new Random();
+    }
+    
+    public boolean insert(int val) {
+        if(indexMap.containsKey(val)){
+            return false;
+        }
+
+        indexMap.put(val, nums.size());
+        nums.add(val);
+        return true;
+    }
+    
+    public boolean remove(int val) {
+        if(!indexMap.containsKey(val)){
+            return false;
+        }
+
+        int currentIndex = indexMap.get(val);
+        int lastIndex = nums.size()-1;
+        if(currentIndex != lastIndex){
+            int lastVal = nums.get(lastIndex);
+            nums.set(currentIndex, lastVal);
+            indexMap.put(lastVal, currentIndex);
+        }
+        nums.remove(lastIndex);
+        indexMap.remove(val);
+        return true;
+
+    }
+    
+    public int getRandom() {
+        return nums.get(random.nextInt(nums.size()));
+    }
+
+}
